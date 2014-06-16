6c3385a416aad51eaeac3a12367b498bbab890e8
Reto Omlin <rohmux@gmail.com>
2014-06-16 20:54:54 +0200

    Initial release

---
+++ run_analysis.R
@@ -0,0 +1,7 @@
+## Reading Features
+colheaders <- read.table("data/features.txt")
+
+## Reading Testset
+xtest <- read.table("data/test/X_test.txt")
+ytest <- read.table("data/test/y_test.txt")
+stest <- read.table("data/test/subject_test.txt")
