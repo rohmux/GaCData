7a7568be5a92edacc1084664bc023d4c82968777
Reto Omlin <rohmux@gmail.com>
2014-06-16 20:59:02 +0200

    added comments updated README.md

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ GaCData
 =======
 
 Repo for the Getting and Cleaning Data Coursera course
+
+Date analyzed for this project
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
--- run_analysis.R
+++ run_analysis.R
@@ -2,6 +2,11 @@
 colheaders <- read.table("data/features.txt")
 
 ## Reading Testset
+# Samples
 xtest <- read.table("data/test/X_test.txt")
+
+# Sampleactivity
 ytest <- read.table("data/test/y_test.txt")
+
+# Samplesubject
 stest <- read.table("data/test/subject_test.txt")
