bf16661db635ba4cc29afda9d5278e3a96784e52
Reto Omlin <rohmux@gmail.com>
2014-06-17 20:44:59 +0200

    begin merging data

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ GaCData
 
 Repo for the Getting and Cleaning Data Coursera course
 
-Date analyzed for this project
+Data analyzed for this project
 https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
--- run_analysis.R
+++ run_analysis.R
@@ -3,10 +3,18 @@ colheaders <- read.table("data/features.txt")
 
 ## Reading Testset
 # Samples
-xtest <- read.table("data/test/X_test.txt")
+xtest <- data.frame(read.table("data/test/X_test.txt"))
 
 # Sampleactivity
 ytest <- read.table("data/test/y_test.txt")
 
 # Samplesubject
 stest <- read.table("data/test/subject_test.txt")
+
+# Assigning the Features Column 2 as Variablenames to xtest (Column 1 is just a numeration, Column 2 is the real name)
+names(xtest) <- tolower(colheaders[,2])
+
+# Merge Subject and Activity with Samples
+xtest <- cbind(stest, ytest, xtest)
+names(xtest)[1] <- "subjectnr"
+names(xtest)[2] <- "activity"
