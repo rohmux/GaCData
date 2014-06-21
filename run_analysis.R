37594cd0d09a2cb5d73a405d3806213d8c3943a5
Reto Omlin <rohmux@gmail.com>
2014-06-21 23:04:37 +0200

    finalized script, updated README.md

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,16 @@
+library(reshape2)
 ## Reading Features and fixing names (remove "-" "()")
 colheaders <- read.table("data/features.txt")
 colheaders[,2] <- gsub("[-(),]", "", colheaders[,2])
+colheaders[,2] <- gsub("fBody", "frequencybody", colheaders[,2])
+colheaders[,2] <- gsub("tBody", "timedbody", colheaders[,2])
+colheaders[,2] <- gsub("Acc", "acceleration", colheaders[,2])
+colheaders[,2] <- gsub("Gyro", "gyroscope", colheaders[,2])
+colheaders[,2] <- gsub("tGravity", "timedgravity", colheaders[,2])
+colheaders[,2] <- gsub("bodybody", "body", colheaders[,2])
+
+## Reading activity labels
+actlabels <- read.table("data/activity_labels.txt")
 
 getdataset <- function(type){
     path <- paste("data/",type,"/", sep ="")
@@ -19,14 +29,51 @@ getdataset <- function(type){
     
     # Stripping out unwanted Variables
     toMatch <- c("mean", "std")
-    names(xset[, grep(paste(toMatch,collapse="|"), names(xset))])
+    xset <- xset[, grep(paste(toMatch,collapse="|"), names(xset))]
     
     # Merge Subject and Activity with Samples
     xset <- cbind(subjectnr, yset, xset)
     names(xset)[1] <- "subjectnr"
     names(xset)[2] <- "activity"
+    
+    # Replace activity numbers by its label
+    for(i in 1:6){
+        replacestring <- actlabels[i,2]
+        xset$activity[xset$activity == i] <- as.character(replacestring)
+    }
+    
+    # Add Column of set type (train or test)
+    xset <- data.frame(settype = type, xset)
+    
     xset
+}
+
+# Merges Training and Testset
+mergeset <- function(set1, set2){
+    mergedset <- rbind(set1, set2)
+    
+    mergedset
+}
+
+# Just write the dataset into a file calles meanvalues.txt
+writeset <- function(ds){
+    if(!file.exists("data/meanvalues.txt")){
+        write.csv(ds, file="data/meanvalues.txt")
+        "file written"
+    }
+    else{
+        "file exists"
+    }
+}
+
+createtidyset <- function(ds){
+    moltenset <- melt(ds, id=c("activity", "subjectnr", "settype"), measure.vars=c(4:89))
+    tidyset <- cast(moltenset, subjectnr+activity~variable, mean)
+    tidyset
 }
 
 trainset <- getdataset("train")
 testset <- getdataset("test")
+mergedset <- mergeset(trainset, testset)
+tidyset <- createtidyset(mergedset)
+writeset(tidyset)
